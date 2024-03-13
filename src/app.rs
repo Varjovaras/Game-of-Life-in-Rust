708c38f5fdc2ff65bc27a53a742f6ee462d7045e
Kristjan Rajaniemi <kristjan.rajaniemi@live.fi>
2024-03-13 21:56:55 +0200

    Tailwind initialization

--- src/app.rs
+++ src/app.rs
@@ -5,7 +5,9 @@ pub fn App() -> impl IntoView {
     let (count, set_count) = create_signal(0);
 
     view! {
-        <button
+
+        <h1 class="text-green-400 text-4xl text-center">alimesami</h1>
+        <button class="border border-fuchsia-500"
             on:click=move |_| {
                 set_count.update(|n| *n += 1);
             }
